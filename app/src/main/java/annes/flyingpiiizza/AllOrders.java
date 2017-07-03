053ef03ec03b93b5188a2cc5ca9412d31eb9c8d9
A. S <ass97@online.de>
2017-07-03 21:09:03 +0200

    Added intent and back button

--- app/src/main/java/annes/flyingpiiizza/AllOrders.java
+++ app/src/main/java/annes/flyingpiiizza/AllOrders.java
@@ -1,5 +1,6 @@
 package annes.flyingpiiizza;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -11,6 +12,7 @@ import android.widget.Button;
 public class AllOrders extends AppCompatActivity {
 
     private Button back;
+    private Button newOrder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,11 +22,20 @@ public class AllOrders extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         back = (Button) findViewById(R.id.backButton);
+        newOrder = (Button) findViewById(R.id.newOrderButton);
 
         back.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
                 finish();
+            }
+        });
+
+        newOrder.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AllOrders.this, CreateOrder.class);
+                startActivity(intent);
             }
         });
     }
