6a134a1e541d6eb26b5a7c4575c960c8cc73a734
A. S <ass97@online.de>
2017-07-04 08:40:59 +0200

    Added Button Definitions to Create Order Class

--- app/src/main/java/annes/flyingpiiizza/createOrder.java
+++ app/src/main/java/annes/flyingpiiizza/createOrder.java
@@ -7,10 +7,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class CreateOrder extends AppCompatActivity {
 
     private Button back;
+    private ListView proposals;
+    private Button createOrder;
+    private EditText orderName;
+    private EditText dishNameSearch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +27,11 @@ public class CreateOrder extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         back = (Button) findViewById(R.id.buttonBack);
+        proposals = (ListView) findViewById(R.id.proposals);
+        createOrder = (Button) findViewById(R.id.createOrder);
+        orderName = (EditText) findViewById(R.id.orderName);
+        dishNameSearch = (EditText) findViewById(R.id.dishNameSearch);
+
 
         back.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -27,6 +39,15 @@ public class CreateOrder extends AppCompatActivity {
                 finish();
             }
         });
+
+        createOrder.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        proposals.setVisibility(View.INVISIBLE);
     }
 
 }
